ea097f5a81b81de9956e2473b86c56dbdfed6c3b
Rubenlopez-2803 <rlopiba2803@g.educaand.es>
2026-03-01 00:25:07 +0100

    Create 19.1.py

---
+++ tarea 5/19.1.py
@@ -0,0 +1,12 @@
+def es_par(num):
+    if num % 2 == 0:
+        print(num, "es par")
+    else:
+        print(num, "es impar")
+
+# Llamadas a la función
+es_par(2)
+es_par(5)
+es_par(8)
+es_par(11)
+es_par(17)
