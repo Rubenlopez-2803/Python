e07d09b0717fb34a32fb2fead8b07d48d9dd3852
Rubenlopez-2803 <rlopiba2803@g.educaand.es>
2026-02-28 22:14:07 +0100

    Add files via upload

---
+++ tarea 16.3.py
@@ -0,0 +1,7 @@
+frase = 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme'
+contador = 0
+for letra in frase:
+    print(letra, end='')
+    contador = contador + 1
+    if contador == 30:
+        break
