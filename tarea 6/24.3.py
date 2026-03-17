c0b0f9c67fbcccfce169c9da1c2e2ae41900afa4
Rubenlopez-2803 <rlopiba2803@g.educaand.es>
2026-03-18 00:28:03 +0100

    Create 24.3.py

---
+++ tarea 6/24.3.py
@@ -0,0 +1,5 @@
+e = 2.718281828       # Número de punto flotante que ocupa 10 espacios y tiene 3 decimales
+print(f'{e:10.3f}')
+print(f'{e:010.5f}')  # Cinco decimales y ceros a la izquierda.
+print(f'{e:10.0f}')   # Igual que el primero, con cero decimales
+print(f'{e:10.4e}')   # Igual que el primero, pero en notación científica
